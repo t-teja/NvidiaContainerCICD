281cb355794f55afe2ec77c15a93266fd707fe8e
Teja <tejalabs@outlook.com>
2026-02-01 21:59:04 +0530

    test1

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jetson Orin CI/CD working🚀"
+    return "Hello from Jetson Orin CI/CD working auto deployment test🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
