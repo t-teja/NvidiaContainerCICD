89c8178d6c9110142454d3bdc0540b541f9a4734
Teja <tejalabs@outlook.com>
2026-02-01 22:04:55 +0530

    testing container restart

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jetson Orin CI/CD working auto deployment test🚀"
+    return "Testing docker prune and down before building and running docker container"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
