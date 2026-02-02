ddf438e52a5d4d0955f6878e5b3575eb9a3bcc31
Teja <tejalabs@outlook.com>
2026-02-02 14:30:32 +0530

    branch update

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Testing docker prune and down before building and running docker container"
+    return "branch PR deploy test CICD pipeline"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
